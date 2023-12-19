97b6928fbd7632d7cc56993d234ea2901f35ff4a
Raj Aryan <arpittaryan2k12@gmail.com>
2023-12-19 23:10:10 +0530

    Auto Wiring Using @annotation on field >> can't make field final annotation on setter Method>> can't make fields final annotation on constructor>> can make fields final Circular Dependency Auto wiring of same type of bean using: parameter or variable name @Primary @Qualifier

---
+++ example11/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,43 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name = "Lucy";
+    /*
+    * The @Autowired annotation marks onn a field, constructor, Setter method
+    * is used to auto-wire that beans that is 'injecting beans' (Objects) at runtime
+    * by Spring Dependency Injection mechanism.
+    * */
+//    @Autowired
+    private Vehicle vehicle;
+
+    @Autowired
+    public Person(Vehicle vehicle) {
+        System.out.println("Person bean created by Spring");
+        this.vehicle = vehicle;
+    }
+
+    public Person(String name, Vehicle vehicle) {
+        System.out.println("Default Constructor"+name+" "+vehicle.getName());
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+//    @Autowired
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
